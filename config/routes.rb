a4e69ac9c4d2b015d6b78b1df15f4e105d1cdc02
Pauline Gaget <lupiane@hotmail.com>
2017-09-09 21:41:39 +0200

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,13 @@ Rails.application.routes.draw do
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root to: 'pages#home'
+  get 'contact', to: 'pages#contact'
+  resources :composts
+  resources :messages, only: [:create]
+  resources :users, only: [] do
+    member do
+      get 'composts', to: 'users#composts'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
