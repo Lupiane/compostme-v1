0fdb2ab356777934eeda8da88e72414b024b2689
Pauline Gaget <lupiane@hotmail.com>
2017-09-11 17:27:30 +0200

    validations and associations in database and changed default value of admin to false in user

--- app/models/compost.rb
+++ app/models/compost.rb
@@ -1,3 +1,5 @@
 class Compost < ApplicationRecord
   belongs_to :user
+  has_many :reviews, dependent: :destroy
+  validates :type, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170909200038) do
+ActiveRecord::Schema.define(version: 20170911152448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 20170909200038) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.string "provider"
     t.string "uid"
     t.string "facebook_picture_url"
